378cdcaf4f2856ba1df88f32782feaca897487f5
Drew Folta <drew@folta.net>
2019-05-20 13:00:10 -0700

    freshened travis builds

--- ci-boards.sh
+++ ci-boards.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-BOARDS=$(pio boards | grep SAMD21G18A | awk '{ print $1 }' | grep -v adafruit_circuitplayground_m0 | grep -v moteino_zero)
-##### 2018-Aug-19:  adafruit_circuitplayground_m0 fails with a bunch of
-# fatal error: sam.h: No such file or directory
-##### 2018-Aug-19:  moteino_zero fails with a bunch of
-# fatal error: variant.h: No such file or directory
+BOARDS=$(pio boards | grep SAMD21G18A | awk '{ print $1 }')
 
 FAILED=""
 for BOARD in $BOARDS; do
